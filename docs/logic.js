d23199ab6b0afcdc8345ead46925de4cf446c741
John Davidson <davidov.i@gmail.com>
2021-08-03 15:49:11 +0300

    Website updates

--- docs/logic.js
+++ docs/logic.js
@@ -255,7 +255,7 @@ function calculateMedians() {
   });
 }
 
-function useFixedColors(use) {
+function useFixedColors() {
   let colors = ['#3366CC', '#DC3912', '#FF9900', '#109618', '#990099', '#3B3EAC', '#0099C6', '#DD4477', '#66AA00', '#B82E2E', '#316395', '#994499', '#22AA99', '#AAAA11', '#6633CC', '#E67300', '#8B0707', '#329262', '#5574A6', '#3B3EAC'];
   let counter = 0;
   s.forEach((o) => {o.c = colors[++counter % colors.length];});
